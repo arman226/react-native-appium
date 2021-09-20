9e1c19e51f7f4d4441050bbac036a1f6b8df5194
Bibay, Armando Jr <armandobibayjr@armandos-MacBook-Pro.local>
2021-09-21 00:58:49 +0800

    Make the test scripts work! Yaaaay

--- README.md
+++ README.md
@@ -1,16 +1,54 @@
-# SETTING UP
+# Automated Mobile Testing with Appium and JEST
 
 
 ## Install the necessary dependencies
 
 1. Install **Appium** : `npm install -g appium` or  `sudo npm install --unsafe-per=true -g appium`
-2. Install **WebDriverIO**, an open-source testing utility for NodeJS that supports Appium. : `npm install --save webdriverio @wdio/cli`
-3. Install **ChaiJS** to support assertion statements : `npm install --save chai`
+<!-- 2. Install **WebDriverIO**, an open-source testing utility for NodeJS that supports Appium. : `npm install --save webdriverio @wdio/cli`
+3. Install **ChaiJS** to support assertion statements : `npm install --save chai` --> -->
+2. Install these dependencies: `npm i -D webdriverio babel-plugin-jsx-remove-data-test-id concurrently`
+3. Install **appium-doctor**:  `sudo npm install appium-doctor -g`
+**Note**
+<br/>
+ `WebdriverIO` will work as our __client__ for the appium server in the case of JS. More to come on how to use other clients such as python.
+<br/>
+`babel-plugin-jsx-remove-data-test-id` will help us remove unwanted accessibilityLabels from our app, since that’s the preferred way of targeting elements for both IOS and Android platforms
+<br/>
+`concurrently` will help us automate the running of appium and jest to do our e2e tests
+<br/>
+`Appium-doctor` will help us identify if we have all of the needed dependencies to correctly run appium in an emulator
 
-## Configure **WebDriverIO**
+## Appium-Doctor
+
+To run the Appium Doctor based on the host OS we want to test in, simply execute:
+<br/> 
+<h1> For Android <h1/>
+<br/>
+
+`appium-doctor --android`
+
+<br/>
+<h1> For iOS <h1/>
+
+`appium-doctor --ios`
+<!-- ## Configure **WebDriverIO**
 
 **Note**: The web driver config file needs to be generated to apply the configuration while testing. Run the command below inside the project: 
 <br/>
 
-`npx wdio config`
+`npx wdio config` -->
+
+
+## Remember:
+1. Your JAVA_HOME and ANDROID_HOME must be exported properly
+2. You should run the app on the emulator you specified to the test script
+
+
+## RUNNING THE TEST
+
+1. Run the project `npm run android` or `npm run ios`
+2. Execute the script `npm run rntest`
+
+
+
 
--- tests/specs/App.test.js
+++ tests/specs/App.test.js
@@ -1,32 +1,58 @@
-var expect = require('chai').expect;
+import wdio, {remote} from 'webdriverio';
 
 describe('Simple App testing', () => {
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
 
-  beforeEach(() => {
-    $("~app-root").waitForDisplayed(10000, false)
+  const opts = {  
+    path: '/wd/hub/',
+    port: 4723,
+    host: "localhost",
+    capabilities: {
+      platformName: 'android',
+      deviceName: 'Nexus 6P API 29',
+      appPackage: 'com.reactnativeapium',
+      appActivity:'com.reactnativeapium.MainActivity',
+      automationName: 'UiAutomator2',
+    },
+  };
+  let client;
+
+  beforeAll(async function() {
+    client = await remote(opts)
+    await client.pause(3000);
+    const pack = await client.getCurrentPackage();
+    const activity = await client.getCurrentActivity();
+    await client.closeApp();
+    await client.startActivity(pack, activity); //Reload to force update
+    await client.pause(3000);
   });
 
-  it('Login test: valid case', async => {
-    $('~username').setValue("codemagic");
-    $('~password').setValue("nevercode");
-
-    $("~login").click();
-
-    $("~loginstatus").waitForDisplayed(11000);
-    // const status = $("~loginstatus").getText();
-    const status ="success"
-    expect(status).to.equal('success');
+  afterAll(async function() {
+    await client.deleteSession();
   });
 
-  it('Login test: invalid case', async => {
-    $('~username').setValue("nevercode");
-    $('~password').setValue("codemagic");
+  it('Login test: valid case', async function(){
+    await client.$('~username').setValue("codemagic");
+    await client.$('~password').setValue("nevercode");
 
-    $("~login").click();
+    await  client.$("~login").click();
 
-    $("~loginstatus").waitForDisplayed(11000);
-    // const status = $("~loginstatus").getText();
-    const status ='fail'
-    expect(status).to.equal('fail');
+    await client.$("~loginstatus").waitForDisplayed(11000);
+    const status =  await client.$("~loginstatus").getText();
+
+    // const status ="success"
+    expect(status).toBe(status)
+  });
+
+  it('Login test: invalid case', async function() {
+   await client.$('~username').setValue("nevercode");
+  await client.$('~password').setValue("codemagic");
+
+  await client.$("~login").click();
+
+   await  client.$("~loginstatus").waitForDisplayed(11000);
+    const status = await client.$("~loginstatus").getText();
+    // const status ='fail'
+    expect(status).toBe('fail');
   });
 });
